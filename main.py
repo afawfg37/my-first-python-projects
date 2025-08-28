05b2514126407d5d5458d992965e090cba54b30a
Arda Uluışık <arda32092@gmail.com>
2025-08-28 16:09:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from pathlib import Path
 # Kullanıcının ev dizini
 ev = Path.home()
 
-# Masaüstü yolu
+
 masaustu = ev / "Masaüstü"
 
 print(masaustu)
 
-# Örnek: Masaüstüne dosya yaz
+
 dosya = masaustu / "deneme.txt"
-dosya.write_text("Merhaba kral, ben masaüstündeyim!")
+dosya.write_text("Merhaba ben masaüstündeyim!")
 
